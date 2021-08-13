f6f197114d883f5224273464056e753223e6356c
Arunesh7-md <85237692+Arunesh7-md@users.noreply.github.com>
2021-08-13 11:04:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+   //Initialising Engine
+  engine = Engine.create();
+  world = engine.world;
+	
+   //Create Player Base and Computer Base Object
+   
+
+
+ }
+
+function draw() {
+
+  background(180);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+   //Display Playerbase and computer base 
+  playerBase = new Playerbase(300, random(450, height - 300),180, 150)
+
+   //display Player and computerplayer
+   player = new Player(285,playerBase.body.position.y - 150, 50, 180)
+
+
+}
